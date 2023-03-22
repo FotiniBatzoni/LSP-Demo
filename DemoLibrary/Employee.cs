46f026f43f903cd05e346fcdfcfc0f727319749a
fotini <f.batzoni@gmail.com>
2023-03-22 13:16:58 +0200

    Completing the Employee class

--- DemoLibrary/Employee.cs
+++ DemoLibrary/Employee.cs
@@ -10,5 +10,21 @@ namespace DemoLibrary
     {
         public string FirstName {get;set;}
         public string LastName { get; set; }
+
+        public Employee Manager { get; set; } = null;
+
+        public decimal Salary { get; set; }
+
+        public virtual void AssignManager(Employee manager)
+        {
+            Manager = manager;
+        }
+
+        public virtual void CalculatePerHourRate(int rank)
+        {
+            decimal baseAmount = 12.5M;
+
+            Salary = baseAmount + (rank * 2);
+        }
     }
 }
