68c106187c57b09adb51519faecdd21c23f6ede2
fotini <f.batzoni@gmail.com>
2023-03-22 13:23:19 +0200

    Adding methods to CEO class

--- DemoLibrary/CEO.cs
+++ DemoLibrary/CEO.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary
 {
-    public class CEO
+    public class CEO : Employee
     {
-        public string FirstName { get; set; }   
-        public string LastName { get; set; }
+        public override void CalculatePerHourRate(int rank)
+        {
+            decimal baseAmount = 150M;
 
+            Salary = baseAmount * rank;
+        }
+
+        public override void AssignManager(Employee manager)
+        {
+           throw new InvalidOperationException("The CEO has no manager");
+        }
+
+        public void GenertePerformanceReview()
+        {
+            Console.WriteLine("I'm reviewing a direct report's performance");
+        }
     }
 }
