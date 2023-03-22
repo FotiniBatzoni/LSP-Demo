303f9dc6df9db759933fede963a793f9abec8329
fotini <f.batzoni@gmail.com>
2023-03-22 13:49:56 +0200

    Adding BaseEmployee class

---
+++ DemoLibrary/BaseEmployee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class BaseEmployee : IEmployee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public decimal Salary { get; set; }
+
+
+        public virtual void CalculatePerHourRate(int rank)
+        {
+            decimal baseAmount = 12.5M;
+
+            Salary = baseAmount + (rank * 2);
+        }
+    }
+}
