d44329e4bf5cf7875be33f509c76b1f2953817f1
fotini <f.batzoni@gmail.com>
2023-03-22 13:50:55 +0200

    Refactoring the Employee class

--- DemoLibrary/Employee.cs
+++ DemoLibrary/Employee.cs
@@ -6,25 +6,15 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary
 {
-    public class Employee : IEmployee
+    public class Employee : BaseEmployee
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-
         public Employee Manager { get; set; } = null;
 
-        public decimal Salary { get; set; }
 
         public virtual void AssignManager(Employee manager)
         {
             Manager = manager;
         }
 
-        public virtual void CalculatePerHourRate(int rank)
-        {
-            decimal baseAmount = 12.5M;
-
-            Salary = baseAmount + (rank * 2);
-        }
     }
 }
